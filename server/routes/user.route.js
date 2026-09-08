f4675d4904c9db6386c78f50f8205df3f3f05900
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2026-09-08 23:18:21 +0100

    Implement Auth Limiter to have better security feature for a user.

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { authLimiter } = require("../middleware/auth.limiter")
+
 
 const { registerUser, getAllUsers, loginUser } = require("../controllers/user.controller");
 const { validate } = require("../middleware/requestsValidation.middleware")
 
 const { userValidationSchema, userLoginValidation } = require("../validations/users.validation")
 
-router.post("/registerUser", validate(userValidationSchema, "body"), registerUser);
+router.post("/registerUser", authLimiter, validate(userValidationSchema, "body"), registerUser);
 router.get("/users", getAllUsers);
-router.post("/login", validate(userLoginValidation, "body"), loginUser);
+router.post("/login", authLimiter, validate(userLoginValidation, "body"), loginUser);
 
 
 module.exports = router;
